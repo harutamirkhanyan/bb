c7760e9ec0748bcd39af12c2f0a77d926b15f63a
Harut <har.amirkhanyan@gmail.com>
2023-04-05 10:26:41 +0400

    Change hystorymode

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage/HomePage.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomePage, alias: '/home' },
     // { path: 'store', name: 'store', component: Store },
