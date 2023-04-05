7877b334ac179d6a0576223615f884b0f8bef39d
Harut <har.amirkhanyan@gmail.com>
2023-04-06 02:02:40 +0400

    Optimization imgloading in animations

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/bb/',
-  chainWebpack: config => {
-    config.plugins.delete('preload')
-    config.plugins.delete('prefetch')
-  }
 })
