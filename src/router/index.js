b44b36c8d027bcf20ab80c024fe65d68a29a959d
Harut <har.amirkhanyan@gmail.com>
2023-04-09 12:02:20 +0400

    Add compoent store & edit navbar logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage/HomePage.vue'
 import ProjectsPage from '@/views/ProjectsPage/ProjectsPage.vue'
+import BbStore from '@/views/BbStore/BbStore.vue'
 
 export default createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomePage, alias: '/home' },
     { path: '/projects', name: 'projects', component: ProjectsPage },
-    // { path: '/', name: 'home', component: HomePage },
+    { path: '/store', name: 'store', component: BbStore },
     // { path: '/', name: 'home', component: HomePage },
     // { path: '/', name: 'home', component: HomePage }
   ]
---
+++ src/views/BbStore/BbStore.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.store Magazine
+    
+</template>
+
+<script>
+// import { ref } from 'vue';
+
+export default {
+  name: 'BbStore',
+  components: {  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style src="./_bbStore.scss" lang="scss"></style>
