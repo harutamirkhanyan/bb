8172215917a4b92a77649fee2fc1de02a8191aa7
Harut <har.amirkhanyan@gmail.com>
2023-04-11 19:31:10 +0400

    finished store page

--- src/views/StorePage/StorePage.vue
+++ src/views/StorePage/StorePage.vue
@@ -1,39 +1,41 @@
 <template lang="pug">
 .store
-  .store__wrapper.container
-    h2.store__title MAGAZIN
-    p.store__text {{text}}
-    span.store__verticalText {{verticalText}}
-  .store__cards.container
-    .store__container.store__container--two
-        .store__card.store__card--four(v-if="storeItems[0]")
-          StoreCard(v-bind="storeItems[0]" animationDirection="right")
-        .store__card.store__card--five(v-if="storeItems[1]")
-          StoreCard(v-bind="storeItems[1]" animationDirection="left")
-    .store__container
-      .store__card.store__card--one(v-if="storeItems[2]")
-        StoreCard(v-bind="storeItems[2]" animationDirection="left")
-      .store__cardWrapper
-        .store__card.store__card--two(v-if="storeItems[3]")
-          StoreCard(v-bind="storeItems[3]" animationDirection="right")
-        .store__card.store__card--three(v-if="storeItems[4]")
-          StoreCard(v-bind="storeItems[4]" animationDirection="right")
-    .store__container.store__container--reverse
-      .store__card.store__card--one(v-if="storeItems[5]")
-        StoreCard(v-bind="storeItems[5]" animationDirection="left")
-      .store__cardWrapper
-        .store__card.store__card--two(v-if="storeItems[6]")
-          StoreCard(v-bind="storeItems[6]" animationDirection="right")
-        .store__card.store__card--three(v-if="storeItems[7]")
-          StoreCard(v-bind="storeItems[7]" animationDirection="right")
-  .alsoInterested__container
-    
+  .store__wrapper
+    .store__content.container
+      h2.store__title MAGAZIN
+      p.store__text {{text}}
+      span.store__verticalText {{verticalText}}
+    .store__cards.container
+      .store__container.store__container--two
+          .store__card.store__card--four(v-if="storeItems[0]")
+            StoreCard(v-bind="storeItems[0]" animationDirection="right")
+          .store__card.store__card--five(v-if="storeItems[1]")
+            StoreCard(v-bind="storeItems[1]" animationDirection="left")
+      .store__container
+        .store__card.store__card--one(v-if="storeItems[2]")
+          StoreCard(v-bind="storeItems[2]" animationDirection="left")
+        .store__cardWrapper
+          .store__card.store__card--two(v-if="storeItems[3]")
+            StoreCard(v-bind="storeItems[3]" animationDirection="right")
+          .store__card.store__card--three(v-if="storeItems[4]")
+            StoreCard(v-bind="storeItems[4]" animationDirection="right")
+      .store__container.store__container--reverse
+        .store__card.store__card--one(v-if="storeItems[5]")
+          StoreCard(v-bind="storeItems[5]" animationDirection="left")
+        .store__cardWrapper
+          .store__card.store__card--two(v-if="storeItems[6]")
+            StoreCard(v-bind="storeItems[6]" animationDirection="right")
+          .store__card.store__card--three(v-if="storeItems[7]")
+            StoreCard(v-bind="storeItems[7]" animationDirection="right")
+  .store__recommendedContent.container
+    RecommendedContent(:description='recomendatetDescription' :title='recomendatetTitle' :items='recomendatetItems')
 
 </template>
 
 <script>
 import { ref } from 'vue';
 import StoreCard from '@/components/StoreCard/StoreCard.vue';
+import RecommendedContent from '@/components/RecommendedContent/RecommendedContent.vue';
 import magazineOne from '@/assets/img/magazineOne.jpg';
 import magazineTwo from '@/assets/img/magazineTwo.jpg';
 import magazineThree from '@/assets/img/magazineThree.jpg';
@@ -41,10 +43,13 @@ import magazineFour from '@/assets/img/magazineFour.jpg';
 import magazineFive from '@/assets/img/magazineFive.jpg';
 import magazineSix from '@/assets/img/magazineSix.jpg';
 import magazineSeven from '@/assets/img/magazineSeven.png';
+import recommendedOne from '@/assets/img/recommendedOne.jpg';
+import recommendedTwo from '@/assets/img/recommendedTwo.jpg';
+import recommendedThree from '@/assets/img/recommendedThree.jpg';
 
 export default {
   name: 'BbStore',
-  components: { StoreCard },
+  components: { StoreCard, RecommendedContent },
   setup() {
     const verticalText = ref('LOREMIPSUMDOLORSITAMET/CONSECTEERADIPISCINGELIT');
     const text = ref(
@@ -100,12 +105,50 @@ export default {
         src: magazineOne,
       },
     ]);
+
+    const recomendatetItems = ref([
+      {
+        id: 1,
+        title: 'Zukunftsweisendes Leben in Garching',
+        name: 'FVTVRIA',
+        src: recommendedOne,
+        alt: 'FVTVRIA',
+        more: 'MEHR ERFAHREN',
+      },
+      {
+        id: 2,
+        title: 'Bauwerk in der B(l)ooming City Grafi ng',
+        name: 'S’PLATZL',
+        src: recommendedTwo,
+        alt: 'S’PLATZL',
+        more: 'MEHR ERFAHREN',
+      },
+      {
+        id: 2,
+        title: 'Urbane Agrikultur zum Wohnen in Freising',
+        name: 'BEE FREE',
+        src: recommendedThree,
+        alt: 'BEEFREE',
+        more: 'MEHR ERFAHREN',
+      },
+    ]);
+
+    const recomendatetDescription = ref(
+      'Als Architekt, Bauträger und Projektentwickler hauchen wir besonderen Standorten frischen Wind ein. Erfahren Sie mir über unsere Arbeit'
+    );
+    const recomendatetTitle = ref('Das könnte Sie auch interssieren');
     return {
       verticalText,
       text,
       storeItems,
       magazineSix,
       magazineSeven,
+      recomendatetDescription,
+      recomendatetTitle,
+      recomendatetItems,
+      recommendedOne,
+      recommendedTwo,
+      recommendedThree,
     };
   },
 };
